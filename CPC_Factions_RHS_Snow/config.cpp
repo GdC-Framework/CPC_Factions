abe1114d5296cda97dafbe20a653d995aebeb7f6
Sparfell <arsparfell19@gmail.com>
2018-08-18 22:02:47 +0200

    @ Ajout de classes dans le Cfgpatches

--- CPC_Factions_RHS_Snow/config.cpp
+++ CPC_Factions_RHS_Snow/config.cpp
@@ -5,11 +5,15 @@ class CfgPatches
 	{
 		units[] = 
 		{
-			
+			"CPC_Snow_Backpack_1"
 		};
 		weapons[] = 
 		{
-
+			"CPC_Snow_Uniform_1",
+			"CPC_Snow_Uniform_2",
+			"CPC_Snow_Vest_1",
+			"CPC_Snow_Beanie_1",
+			"CPC_Snow_Helmet_1"
 		};
 		requiredVersion = 0.1;
 		requiredAddons[] = {"CPC_Factions_RHS_Main","A3_Characters_F","A3_Weapons_F_Ammoboxes","rhsgref_c_troops","rhs_c_troops","rhsusf_c_troops"};
